66913a6cd4fe98ffb8667ac50e91e57e6664c680
Stephen SIMON <68376360+codewithsimon@users.noreply.github.com>
2025-08-31 00:17:32 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -301,7 +301,7 @@ function App() {
                 <div className="text-xs lg:text-sm text-gray-600">Expert Speakers</div>
               </div>
               <div className="text-center">
-                <div className="text-2xl lg:text-3xl font-bold text-teal-600">10,000 +</div>
+                <div className="text-2xl lg:text-3xl font-bold text-teal-600">2,000 +</div>
                 <div className="text-xs lg:text-sm text-gray-600">Attendees</div>
               </div>
               <div className="text-center">
